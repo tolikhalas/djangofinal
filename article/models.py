eece5ed896a6482cc8fabc3eb4f69093a28fa71c
Anatolii <tolikgalas@gmail.com>
2021-10-16 16:09:30 +0300

    some commit

--- article/models.py
+++ article/models.py
@@ -1,8 +1,51 @@
 import datetime
 from django.db import models
+from django.template.defaultfilters import slugify
+from random import randint
+from transliterate.base import TranslitLanguagePack, registry
+from transliterate import translit
 from django.utils import timezone
 from ckeditor.fields import RichTextField
 # Create your models here.
+
+class UkranianLanguagePack(TranslitLanguagePack):
+    language_code = 'ukr'
+    language_name = 'ukranian'
+    mapping = (
+        u'abvhgdejzyiklmnoprstufABVHGDEJZYIKLMNOPRSTUF',
+        u'абвгґдежзиіклмнопрстуфАБВГҐДЕЖЗИІКЛМНОПРСТУФ'
+    )
+
+    pre_processor_mapping = {
+        u"ye": u"є",
+        u"yy": u"й",
+        u"yi": u"ї",
+        u"kh": u"х",
+        u"ts": u"ц",
+        u"ch": u"ч",
+        u"sh": u"ш",
+        u"shch": u"щ",
+        u"yu": u"ю",
+        u"ya": u"я",
+
+        u"Ye": u"Є",
+        u"Yy": u"Й",
+        u"Yi": u"Ї",
+        u"Kh": u"Х",
+        u"Ys": u"Ц",
+        u"Ch": u"Ч",
+        u"Sh": u"Ш",
+        u"Shch": u"Щ",
+        u"Yu": u"Ю",
+        u"Ya": u"Я",
+    }
+
+    reversed_specific_pre_processor_mapping = {
+        u"ь": u""
+    }
+
+registry.register(UkranianLanguagePack)
+
 
 class Article(models.Model):
 
@@ -13,12 +56,14 @@ class Article(models.Model):
     article_image = models.FileField(blank = True,null = True,verbose_name="Зображення")
     slug = models.SlugField(unique=True, max_length=100)
 
+    random_index = randint(1000, 9999)
+
     def __str__(self):
         return self.title
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            self.slug = slugify(translit(self.title, 'ukr', reversed=True) + " " + str(self.random_index))
         return super(Article, self).save(*args, **kwargs)
 
     def published_recently(self):
