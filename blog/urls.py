7063dd2720ed0a0f32edd7d50ec438bfe0263e08
Anatolii <tolikgalas@gmail.com>
2021-10-18 00:34:49 +0300

    some commit

--- blog/urls.py
+++ blog/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
@@ -27,6 +28,7 @@ urlpatterns = [
     path('articles/',include("article.urls")),
     path('user/',include("user.urls")),
 
-    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
